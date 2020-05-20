a9efa44311ee5467113498b59812ddeb9cf7afff
Arthur Milchior <Arthur@Milchior.fr>
2020-05-21 01:20:27 +0200

    remove support for older anki
    Now "name${i}" should always suceed

--- src/frozen_fields/js.js
+++ src/frozen_fields/js.js
@@ -20,11 +20,6 @@ function setFrozenFields(frozen) {
     $fnames = $(".fname");
     for (var i=0; i<frozen.length; i++) {
         var $td_name = $(`#name${i}`);
-        if ($td_name.length == 0) {
-            // no multi column. Get the i-th fname
-            var td_name = $fnames[i];
-            $td_name = $(td_name);
-        }
         var src = (frozen[i])?src_frozen:src_unfrozen;
         var un_freeze = (frozen[i])?"Unfreeze":"Freeze";
         var img_html = `<img id=i${i} src='${src}' title='${un_freeze} field (${hotkey_toggle_field})' style="height:.9em" onclick='onFrozen(${i});'/>`;
